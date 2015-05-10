b903134248b75499ec1c97fce2cf17ccd89f731c
Tim Martin <timboe@gmail.com>
2015-05-10 09:58:53 -0700

    item get win

--- src/timeSieve.c
+++ src/timeSieve.c
@@ -92,13 +92,14 @@ static void notifyUpdateProc(Layer *this_layer, GContext *ctx) {
   graphics_fill_rect(ctx, b, 6, GCornersAll);
   graphics_context_set_fill_color(ctx, getTrasureColour(s_notifyTreasureID));
   graphics_fill_rect(ctx, GRect(b.origin.x+2, b.origin.y+2, b.size.w-4, b.size.h-4), 6, GCornersAll);
-  graphics_context_set_fill_color(ctx, getTrasureColour(s_notifyTreasureID));
+  graphics_context_set_fill_color(ctx, GColorWhite);
   graphics_fill_rect(ctx, GRect(b.origin.x+4, b.origin.y+4, b.size.w-8, b.size.h-8), 6, GCornersAll);
   // Image placeholder
   graphics_context_set_stroke_color(ctx, GColorBlack);
-  GRect imageRect = GRect(6, 10,  22, 36);
+  graphics_context_set_font_color(ctx, GColorBlack);  
+  GRect imageRect = GRect(b.origin.x+6, b.origin.y+10,  22, 36);
   graphics_draw_rect(ctx, imageRect);
-  graphics_draw_text(ctx, "Treasure!", fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD), GRect(30,10,b.size.w-40,30), GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);
+  graphics_draw_text(ctx, "Treasure!", fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD), GRect(b.origin.x+30, b.origin.y+10,b.size.w-40,30), GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);
 }
 
 void stopNotify(void* data) {
@@ -147,7 +148,7 @@ void create_timeSieve_layer(Window* parentWindow) {
   bitmap_layer_set_bitmap(s_sieveLayer, s_sieveBasic);
   layer_add_child(s_timeSieveLayer, bitmap_layer_get_layer(s_sieveLayer));
 
-  s_notifyLayer = layer_create( GRect(0, 10, window_bounds.size.w, 36) ); // border 10 top and bottom
+  s_notifyLayer = layer_create( GRect(0, 5, window_bounds.size.w, 46) ); // border 5 top and bottom
   layer_set_update_proc(s_notifyLayer, notifyUpdateProc); 
   layer_add_child(s_timeSieveLayer, s_notifyLayer);
 }
