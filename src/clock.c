12f74e946d6bcd1e7965e4e6b05869fa46616fd5
Tim Martin <timboe@gmail.com>
2015-05-16 10:47:14 -0700

    weather start

--- src/clock.c
+++ src/clock.c
@@ -206,7 +206,7 @@ static void clock_update_proc(Layer *this_layer, GContext *ctx) {
   // WEATHER
   GRect wRect = GRect(tank_bounds.origin.x, tank_bounds.origin.y+2, tank_bounds.size.w/3, 25);
   graphics_context_set_text_color(ctx, getLiquidTimeHighlightColour());
-  graphics_draw_text(ctx, s_weatherIcon, *getWeatherFont(), wRect, GTextOverflowModeWordWrap, GTextAlignmentLeft, NULL);
+  graphics_draw_text(ctx, s_weatherIcon, *getWeatherFont(), wRect, GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);
   graphics_context_set_text_color(ctx, GColorBlack);
   wRect.origin.y += 1;
   graphics_draw_text(ctx, s_temperature, fonts_get_system_font(FONT_KEY_GOTHIC_14), wRect, GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);
